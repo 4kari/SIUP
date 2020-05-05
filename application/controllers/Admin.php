81872611254a37c9848fbb6555e8d76db9b24224
syaban <syabansim@gmail.com>
2020-05-05 13:16:16 +0700

    fixing modal in User Management

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,41 +4,42 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Admin extends CI_Controller
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		if($this->session->userdata('level')!=1){
-			redirect('Auth');
+    public function __construct()
+    {
+        parent::__construct();
+        if ($this->session->userdata('level') != 1) {
+            redirect('Auth');
         }
-        $this->load->model('model_admin','model');
-	}
+        $this->load->model('model_admin', 'model');
+    }
 
-    public function data(){
+    public function data()
+    {
         $data['side'] = 'SI-UP Admin';
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
-        $data['active']='default';
-        $data['header']='default';
+        $data['active'] = 'default';
+        $data['header'] = 'default';
         return $data;
     }
     public function index()
     {
-        $data=$this->data();
+        $data = $this->data();
         $data['header'] = 'SI-UP - Dashboard';
         $data['active'] = 'Dashboard';
-        $this->load->view('admin/template/header',$data);
+        $this->load->view('admin/template/header', $data);
         $this->load->view('admin/template/sidebar');
         $this->load->view('admin/template/topbar');
         $this->load->view('admin/index');
         $this->load->view('admin/template/footer');
     }
-    
+
     public function transaksi()
     {
-        $data=$this->data();
+        $data = $this->data();
         $data['header'] = 'SI-UP - Transaksi';
         $data['active'] = 'Data Transaksi';
         $data['transaksi'] = $this->db->get('transaksi')->result_array();
-        $this->load->view('admin/template/header',$data);
+        $this->load->view('admin/template/header', $data);
         $this->load->view('admin/template/sidebar');
         $this->load->view('admin/template/topbar');
         $this->load->view('admin/transaksi');
@@ -46,11 +47,11 @@ class Admin extends CI_Controller
     }
     public function barang()
     {
-        $data=$this->data();
+        $data = $this->data();
         $data['header'] = 'SI-UP - Data Barang';
         $data['active'] = 'Data Barang';
         $data['barang'] = $this->db->get('barang')->result_array();
-        $this->load->view('admin/template/header',$data);
+        $this->load->view('admin/template/header', $data);
         $this->load->view('admin/template/sidebar');
         $this->load->view('admin/template/topbar');
         $this->load->view('admin/barang');
@@ -58,11 +59,12 @@ class Admin extends CI_Controller
     }
     public function Management_user()
     {
-        $data=$this->data();
+        $data = $this->data();
         $data['header'] = 'SI-UP - Data Barang';
         $data['active'] = 'Management User';
         $data['Muser'] = $this->model->getuserdata();
-        $this->load->view('admin/template/header',$data);
+        $data['level'] = $this->db->get('level')->result_array();
+        $this->load->view('admin/template/header', $data);
         $this->load->view('admin/template/sidebar');
         $this->load->view('admin/template/topbar');
         $this->load->view('admin/user');
